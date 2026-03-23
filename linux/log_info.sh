50073cfda77959a630a484e60433f4d9cd61e1b7
Gururaj <gururaj.ceg@gmail.com>
2026-03-23 12:25:08 +0530

    cp

--- linux/log_info.sh
+++ linux/log_info.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # log_info.sh: Usage log_info "message"
+
 log_info() {
-    local msg="$1"
-    echo -e "\033[0;32m$msg\033[0m"
+    printf '\033[0;32m%s\033[0m\n' "$*"
+}
+
+log_cmd() {
+    local rendered=()
+    local arg
+    for arg in "$@"; do
+        rendered+=("$(printf '%q' "$arg")")
+    done
+    log_info "+ ${rendered[*]}"
+}
+
+log_run() {
+    log_cmd "$@"
+    "$@"
 }
